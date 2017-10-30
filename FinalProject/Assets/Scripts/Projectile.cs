03493f2133cc232c6476c5f163e7486ebec4f038
Unknown <gw805@nyu.edu>
2017-10-30 16:49:57 -0400

    Added respawning to projectiles

--- FinalProject/Assets/Scripts/Projectile.cs
+++ FinalProject/Assets/Scripts/Projectile.cs
@@ -7,17 +7,23 @@ using Valve.VR.InteractionSystem;
 public class Projectile : MonoBehaviour
 {
     bool primed;
+    bool dying;
 
+    public Vector3 originPos;
     // Use this for initialization
     void Start()
     {
         primed = false;
+        originPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (transform.position.y < -10f && !dying)
+        {
+            StartCoroutine(Die());
+        }
     }
 
     void HandHoverUpdate(Hand hand)
@@ -42,5 +48,22 @@ public class Projectile : MonoBehaviour
     public void primeProjectile()
     {
         primed = true;
+    }
+
+    IEnumerator Die()
+    {
+        dying = true;
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        GetComponent<Collider>().enabled = false;
+        yield return new WaitForSeconds(3f);
+        transform.position = originPos;
+        dying = false;
+        GetComponent<MeshRenderer>().enabled = true;
+        GetComponent<Rigidbody>().isKinematic = false;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        GetComponent<Collider>().enabled = true;
+        primed = false;
     }
 }
