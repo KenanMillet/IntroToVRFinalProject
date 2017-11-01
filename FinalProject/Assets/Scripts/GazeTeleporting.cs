9dbc288ace3d1e773b4cb751a0dabee29dd2d700
Luvneesh Mugrai <lm3300@nyu.edu>
2017-11-01 17:58:47 -0400

    stuff happen with tp

--- FinalProject/Assets/Scripts/GazeTeleporting.cs
+++ FinalProject/Assets/Scripts/GazeTeleporting.cs
@@ -27,36 +27,38 @@ public class GazeTeleporting : MonoBehaviour {
 		// STEP 1: make a Ray object
 		Ray myRay = new Ray( transform.position, transform.forward );
 
-		// STEP 2: declare the distance
-		float myRayDistance = 20f; // only affect stuff 20 meters near us
-
 		// STEP 3: visualize the Raycast
-		Debug.DrawRay( myRay.origin, myRay.direction * myRayDistance, Color.yellow );
+		Debug.DrawRay( myRay.origin, myRay.direction, Color.yellow );
 
 		// STEP 3.5: setup a RaycastHit object, to hold RaycastHit data
 		RaycastHit myRayHit = new RaycastHit(); // this is just an empty container
 
 		// STEP 4: shoot the Raycast
-		if (Physics.Raycast (myRay, out myRayHit, myRayDistance)) {
+		if (Physics.Raycast (myRay, out myRayHit)) {
 			obj = myRayHit.collider.gameObject;
+            if(obj.name == "Tower")
+            {
+                obj.GetComponent<TowerScript>().counter = 2;
+                Debug.Log("I'm looking at " + myRayHit.collider.name); // the thing the raycast hit
+                Renderer[] r;
+                r = myRayHit.collider.GetComponentsInChildren<Renderer>();
+                foreach (Renderer re in r)
+                {
+                    re.material = red;
+                }
+                if (myController.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad))
+                {
+                    //if (myController.GetTouch (SteamVR_Controller.ButtonMask.Touchpad)) {
+                    SteamVR_Fade.Start(Color.black, 0);
+                    SteamVR_Fade.Start(Color.clear, teleportFadeTime);
 
-			Debug.Log ("I'm looking at " + myRayHit.collider.name); // the thing the raycast hit
-			myRayHit.collider.GetComponent<Renderer> ().material = red;
-			/*SteamVR_Fade.Start (Color.black, 0);
-			SteamVR_Fade.Start (Color.clear, 0.1f);
-*/
-			player.transform.position = new Vector3 (myRayHit.transform.position.x, player.transform.position.y, myRayHit.transform.position.z);
-			if (myController.GetPressDown (SteamVR_Controller.ButtonMask.Touchpad)) {
-				//if (myController.GetTouch (SteamVR_Controller.ButtonMask.Touchpad)) {
-				SteamVR_Fade.Start( Color.clear, 0 );
-				SteamVR_Fade.Start( Color.black, teleportFadeTime );
+                    player.transform.position = new Vector3(myRayHit.transform.position.x, player.transform.position.y, myRayHit.transform.position.z);
+                }
+
+            }
 
 
-			}
-
-		} 
-		if (obj != null) {
-			obj.GetComponent<Renderer> ().material = blue;
-		}
+        } 
+		
 	}
 }
---
+++ FinalProject/Assets/Scripts/TowerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerScript : MonoBehaviour {
+    public Material blue;
+    public int counter = 2;
+	private bool once = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        counter = Mathf.Max(counter - 1, 0);
+		if (counter == 0 && once)
+		{
+			Renderer[] r = GetComponentsInChildren<Renderer>();
+			foreach (Renderer re in r)
+			{
+				re.material = blue;
+			}
+			once = false;
+		}
+		if(counter > 0) once = true;
+	}
+}
