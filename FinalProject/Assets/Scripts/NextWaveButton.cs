741d055c0df855f3f8877e8158d48db2672fc50b
Kenan Millet <kvmillet@gmail.com>
2017-11-27 15:36:54 -0500

    Fixed the shit
    maybe kinda sorta

---
+++ FinalProject/Assets/Scripts/NextWaveButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class NextWaveButton : MonoBehaviour
+{
+	public bool push = false, throwAt = false;
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (push)
+		{
+			Hand hand = collision.gameObject.GetComponent<Hand>();
+			if (hand != null && hand.controller != null && hand.controller.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger)) ++EnemySpawner.waveNo;
+		}
+		else if (throwAt && collision.gameObject.GetComponent<Throwable>()) ++EnemySpawner.waveNo;
+	}
+}
