b643fa8ec1caccb2e2891f80f364a08a3c65f6b3
Unknown <gw805@nyu.edu>
2017-12-06 17:52:50 -0500

    Portals are pretty

--- FinalProject/Assets/Scripts/GoalScript.cs
+++ FinalProject/Assets/Scripts/GoalScript.cs
@@ -7,17 +7,23 @@ public class GoalScript : MonoBehaviour
 	Renderer[] renderers;
 	[SerializeField]
 	Gradient healthColors;
+    [SerializeField]
+    Gradient portalColors;
+    [SerializeField]
+    ParticleSystem portal;
+    ParticleSystem.MainModule main;
 
 	void Start ()
 	{
 		renderers = GetComponentsInChildren<Renderer>();
+        main = portal.main;
 	}
 	
 	void Update ()
 	{
-		foreach(Renderer renderer in renderers)
-		{
+		foreach(Renderer renderer in renderers) {
 			renderer.material.color = healthColors.Evaluate(1.0f - GameManager.lives / (float)GameManager.maxLives);
 		}
-	}
+        main.startColor = portalColors.Evaluate(1.0f - GameManager.lives / (float)GameManager.maxLives);
+    }
 }
